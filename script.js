461e7ab1205921a6f99d3f4fd84f6d327e47bb10
Xebros85 <rorban1@live.com>
2024-09-07 21:57:08 -0500

    Finished!

--- script.js
+++ script.js
@@ -2,21 +2,90 @@
 
 let firstNumber = "";
 let secondNumber = "";
+let operator = "";
+let result = 0;
+let percentNumber = "";
+let operatorCheck = false;
+let solved = false;
 const displayBox = document.querySelector("#display");
 
 function updateDisplay() {
-    displayBox.setAttribute("value", `${firstNumber}`);
+    displayBox.setAttribute("value", `${firstNumber} + " " + ${operator} + " " + ${secondNumber}`);
 }
 
+function clearAll() {
+    firstNumber = "";
+    secondNumber = "";
+    operator = "";
+    result = 0;          
+    displayBox.textContent = "";
+    operatorCheck = false;
+    solved = false;
+    console.log("Cleared Display");    
+}
 
 function appendDisplay(input) {
-    console.log("Input:", input)
-    firstNumber = firstNumber + input;
-    displayBox.textContent = firstNumber;
-    console.log("First Number:", firstNumber);
+    if (solved === false) {
+        checkOperator(input);
+    } else {
+        clearAll();
+        checkOperator(input);
+    }         
 }
 
-function clearAll() {    
-    firstNumber = "";
-    console.log("Cleared Display");
+
+function clearScreen() {
+    displayBox.textContent = "";
 }
+
+// check for operator - if operator is true then store secondNumber, if false, firstNumber
+
+function checkOperator(input) {
+    if (operatorCheck === false) {
+        firstNumber += input;
+        firstNumber = parseInt(firstNumber);
+        displayBox.textContent = firstNumber;
+        updateDisplay();
+    } else if (operatorCheck === true) {
+        secondNumber += input;
+        secondNumber = parseInt(secondNumber);
+        displayBox.textContent = secondNumber;
+        updateDisplay();
+    }
+}
+
+function getOperator(input) {
+    clearScreen();    
+    operator += input;
+    displayBox.textContent = operator;
+    operatorCheck = true;
+    updateDisplay();    
+}
+
+function performCalculation() {
+    console.log(firstNumber, operator, secondNumber);    
+    
+    switch(operator) {
+        case "+":
+            result = firstNumber + secondNumber;
+            break;
+        case "-":
+            result = firstNumber - secondNumber;
+            break;
+        case "*":
+            result = firstNumber * secondNumber;
+            break;
+        case "/":
+            result = firstNumber / secondNumber            
+            break;        
+        default:
+            break;
+    }
+    solved = true;
+
+    let newResult = result.toString();
+    
+    console.log(result, typeof(result), newResult, typeof(newResult));
+    displayBox.textContent = newResult.substring(0, 9);    
+}
+
